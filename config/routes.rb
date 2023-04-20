325b74ccf4c725efa26adaa0f5da2789a6f3769d
Elena Morozova <elenamor2005@icloud.com>
2023-04-20 12:04:24 +0400

    4.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  
   get "/articles", to: "articles#index"
 
   # О подробностях DSL, доступного в этом файле, написано в http://rusrails.ru/routing
